f7ddf1bf82e2d8a63272b656bc9966d71d3e872f
AchaTejasSai <43339050+AchaTejasSai@users.noreply.github.com>
2019-03-25 11:00:02 +0530

    Update CalculatorUnitTest.java

--- Calculator/CalculatorUnitTest.java
+++ Calculator/CalculatorUnitTest.java
@@ -15,7 +15,7 @@ class CalculatorUnitTestCase{
 	public void testOperation() {
 		Calculator c = new Calculator();
 		assertEquals(5, c.Operation(3,2,"Addition"));
-		assertEquals(52, c.Operation(32,20,"Subtraction"));
+		assertEquals(12, c.Operation(32,20,"Subtraction"));
 		assertEquals(68, c.Operation(34,2,"Multiplication"));
         assertEquals(19, c.Operation(38,2,"Division"));
 	}
